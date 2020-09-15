07e99384040aa84f80ef52dc89cc0f979bdc3d0e
hurel <haeginh>
2020-09-16 01:21:52 +0900

    added arguments regarding distance

--- virtual_scanner.cpp
+++ virtual_scanner.cpp
@@ -78,32 +78,27 @@ struct ScanParameters
     int    res_hor;           // horizontal  resolution
     double distance;          // distance in mm
     double screen_dist;       // screen distance in mm
-    //    double viewPoint;          // distance in mm
+    int    rotInterval;       // rotation interval in degree
 };
 
 using namespace std;
 void PrintUsage(){
     cout<<"Usage: ./scanner [options] model.ply viewpoints.ply"<<endl;
     cout<<"[options]"<<endl;
-    cout<<"\t-interval       <double>  : distance intervals (default:1)"<<endl;
-    cout<<"\t-max            <double>  : maximum distance to scan (default:10)"<<endl;
-    cout<<"\t-foi        <vert> <hor>  : foi in degree (default: )"<<endl;
-    cout<<"\t-resolution <vert> <hor>  : resolution in integers (default: )"<<endl;
+    cout<<"\t-dist       <double>  : scan distance in mm (default:3000)"<<endl;
+    cout<<"                          or put three numbers for multiple distances [start] [interval] [#]"<<endl;
+    cout<<"\t-rot        <int>     : rotation interval in degree (default:10)"<<endl;
+    cout<<"\t-path       <string>  : directory path for result files (default: .)"<<endl;
+    exit(1);
 }
 
 void EstimateColor(vtkPolyData* data, vtkIdType cellId, double* point, double* rgb);
 
 int main (int argc, char** argv)
 {
-    //arguements
+    //arguements (file names)
     string modelFileName = argv[argc-2];
     string viewFileName  = argv[argc-1];
-    double interval(1.), maxDist(10.);
- /*   for(int i=1;i<argc-1;i++){
-        if(argv[i]=="-interval") interval = atof(argv[++i]);
-        else if(argv[i]=="-max") maxDist  = atof(argv[++i]);
-        else PrintUsage();
-    }*/
 
     //set scan parameters
     ScanParameters scan;
@@ -111,10 +106,30 @@ int main (int argc, char** argv)
     scan.fov_vert = 59.*pi/180.;
     scan.res_hor  = 1280;
     scan.res_vert  = 720;
-//    scan.distance = 1000; //mm
-//    scan.screen_dist = 5000; //mm
-    scan.distance = 4000; //mm
+    scan.distance = 3000; //mm
+    scan.rotInterval = 10;
     scan.screen_dist = 5000; //mm
+    vector<double> scandists;
+
+    //arguments2
+    string path(".");
+    for(int i=1;i<argc-2;i++){
+        if(string(argv[i])=="-dist"){
+            stringstream ss(argv[++i]);
+            vector<double> inputs;
+            double input;
+            while(ss>>input) inputs.push_back(input);
+            if(inputs.size()==1) scan.distance = inputs[0];
+            else
+                for(int n=0;n<inputs[2];n++)
+                    scandists.push_back(inputs[0]+inputs[1]*n);
+        }
+        else if(string(argv[i])=="-rot")  scan.rotInterval = atoi(argv[++i]);
+        else if(string(argv[i])=="-path") path = argv[++i];
+
+        else PrintUsage();
+    }
+    if(scandists.size()==0) scandists.push_back(scan.distance);
 
     //read ply file
     vtkSmartPointer<vtkPolyData> data;
@@ -169,14 +184,15 @@ int main (int argc, char** argv)
     double vert_interval = vert_len / (double) scan.res_vert;
     cout<<hor_len<<"*"<<vert_len<<endl<<hor_interval<<"*"<<vert_interval<<endl;
 
-    ofstream log("labels.txt");
-
-    //vtkSmartPointer<vtkCellLocator> tree = vtkSmartPointer<vtkCellLocator>::New ();
+    //main loop start
+    if(path!=".") system(("mkdir "+path).c_str());
+    ofstream log(path+"/labels.txt");
     vtkTransform* tr1 = vtkTransform::New ();
     vtkTransform* tr = vtkTransform::New ();
     vtkMatrix4x4* mat = vtkMatrix4x4::New();
     vtkMatrix4x4* mat1 = vtkMatrix4x4::New();
-    for(int i=0;i<viewRays.size();i+=40){
+    for(int i=0;i<viewRays.size();i++){ //viewpoint loop
+
         log<<"viewpoint"<<i<<" "<<viewRays[i][0]<<" "<<viewRays[i][1]<<" "<<viewRays[i][2]<<endl;
         //set viewray and upVector
         double angle = vtkMath::AngleBetweenVectors(viewRays[i], viewray0); //radian
@@ -189,8 +205,9 @@ int main (int argc, char** argv)
         tr1->GetMatrix(mat1);
         vtkMath::Normalize(up1);
 
-        for(int deg = 0; deg<31; deg+=10){
-            cout<<"\rStart Scanning..."<<i<<"/"<<viewRays.size()<<" -deg: "<<deg<<flush;
+        for(int deg = 0; deg<360; deg+=scan.rotInterval){ //rotation loop
+
+            //set up/right vectors
             double up[3], right[3];
             tr->Identity ();
             tr->RotateWXYZ((double)deg, viewRays[i]);
@@ -205,89 +222,94 @@ int main (int argc, char** argv)
             vtkMath::Cross(viewRays[i], up, right);
             vtkMath::Normalize(right);
 
-            //Start!!
-            double eye[3];
-            eye[0] = -viewRays[i][0]*scan.distance;
-            eye[1] = -viewRays[i][1]*scan.distance;
-            eye[2] = -viewRays[i][2]*scan.distance;
-
-            // Generate screen
-            double screen_center[3];
-            for(int n=0;n<3;n++) screen_center[n] = viewRays[i][n] * scan.screen_dist;
-            double screen_leftup[3];
-            for(int n=0;n<3;n++) screen_leftup[n] = screen_center[n]+up[n]*vert_len*0.5 - right[n]*hor_len*0.5;
-
-            pcl::PointCloud<pcl::PointXYZ> screenCloud;
-            for(int vert = 0 ; vert<scan.res_vert;vert++){
-                double left[3] = {screen_leftup[0] - up[0]*vert_interval*(vert+0.5),
-                                  screen_leftup[1] - up[1]*vert_interval*(vert+0.5),
-                                  screen_leftup[2] - up[2]*vert_interval*(vert+0.5)};
-                for(int hor = 0 ; hor<scan.res_hor;hor++){
-                    double point[3] = {left[0]+right[0]*hor_interval*(hor+0.5),
-                                       left[1]+right[1]*hor_interval*(hor+0.5),
-                                       left[2]+right[2]*hor_interval*(hor+0.5)};
-                    screenCloud.push_back(pcl::PointXYZ(point[0], point[1], point[2]));
-                }
-            }
-
-            // Scanning
-            pcl::PointCloud<pcl::PointXYZ> cloud;
-            vector<double> xx, yy, zz;
-            vector<int> rr, gg, bb;
-            double p_coords[3], x[3], t, rgb[3];
-            int subId;
-            for(int vert = 0 ; vert<scan.res_vert;vert++){
-                for(int hor = 0 ; hor<scan.res_hor;hor++){
-                    vtkIdType cellId;
-                    double point[3];
-                    point[0] = screenCloud.at(vert*scan.res_hor+hor).x;
-                    point[1] = screenCloud.at(vert*scan.res_hor+hor).y;
-                    point[2] = screenCloud.at(vert*scan.res_hor+hor).z;
-                    if (tree->IntersectWithLine (eye, point, 0, t, x, p_coords, subId, cellId))
-                    {
-                        EstimateColor(data, cellId, x, rgb);
-                        rr.push_back(floor(rgb[0]+0.5));gg.push_back(floor(rgb[1]+0.5));bb.push_back(floor(rgb[2]+0.5));
-                        xx.push_back(x[0]);yy.push_back(x[1]);zz.push_back(x[2]);
-
-                        //vtkMath::Subtract(x,data->GetCenter(),x);
-                        tr->TransformPoint(x,x);
-                        cloud.push_back (PointXYZ(x[0], x[1], x[2]));
-                    }
-                    //else cloud.push_back (PointXYZ(0,0,0));
-                    else{
-                        rr.push_back(0);gg.push_back(0);bb.push_back(0);
-                        xx.push_back(point[0]);yy.push_back(point[1]);zz.push_back(point[2]);
-                        //vtkMath::Subtract(point,data->GetCenter(),point);
-                        tr->TransformPoint(point,point);
-                        cloud.push_back (PointXYZ(point[0],point[1],point[2]));
-                    }
-                } // Horizontal
-            } // Vertical
-
             log<<to_string(i)+" "+to_string(deg)<<" "<<"upVector: "<<up[0]<<" "<<up[1]<<" "<<up[2]<<endl;
             log<<*mat<<endl;
-            //mat->Print(cout);
-            pcl::PCDWriter writer;
-            writer.writeBinaryCompressed(to_string(i)+"_r"+to_string(deg)+".pcd", cloud);
-            ofstream ofs(to_string(i)+"_r"+to_string(deg)+".ply");
-            ofs<<"ply"<<endl;
-            ofs<<"format ascii 1.0"<<endl;
-            ofs<<"comment exported in vitual_scanner"<<endl;
-            ofs<<"element vertex "<<xx.size()<<endl;
-            ofs<<"property float x"<<endl;
-            ofs<<"property float y"<<endl;
-            ofs<<"property float z"<<endl;
-            ofs<<"property uchar red"<<endl;
-            ofs<<"property uchar green"<<endl;
-            ofs<<"property uchar blue"<<endl;
-            ofs<<"end_header"<<endl;
-            for(int idx=0;idx<xx.size();idx++)
-                ofs<<xx[idx]<<" "<<yy[idx]<<" "<<zz[idx]<<" "<<rr[idx]<<" "<<gg[idx]<<" "<<bb[idx]<<endl;
-            ofs.close();
+
+            for(double dist:scandists){ //distance loop
+                cout<<"\rStart Scanning... -view: "<<i<<"/"<<viewRays.size()<<" -deg: "<<deg<<" -dist: "<<dist<<flush;
+                scan.distance = dist;
+                //Start!!
+                double eye[3];
+                eye[0] = -viewRays[i][0]*scan.distance;
+                eye[1] = -viewRays[i][1]*scan.distance;
+                eye[2] = -viewRays[i][2]*scan.distance;
+
+                // Generate screen
+                double screen_center[3];
+                for(int n=0;n<3;n++) screen_center[n] = viewRays[i][n] * scan.screen_dist;
+                double screen_leftup[3];
+                for(int n=0;n<3;n++) screen_leftup[n] = screen_center[n]+up[n]*vert_len*0.5 - right[n]*hor_len*0.5;
+
+                pcl::PointCloud<pcl::PointXYZ> screenCloud;
+                for(int vert = 0 ; vert<scan.res_vert;vert++){
+                    double left[3] = {screen_leftup[0] - up[0]*vert_interval*(vert+0.5),
+                                      screen_leftup[1] - up[1]*vert_interval*(vert+0.5),
+                                      screen_leftup[2] - up[2]*vert_interval*(vert+0.5)};
+                    for(int hor = 0 ; hor<scan.res_hor;hor++){
+                        double point[3] = {left[0]+right[0]*hor_interval*(hor+0.5),
+                                           left[1]+right[1]*hor_interval*(hor+0.5),
+                                           left[2]+right[2]*hor_interval*(hor+0.5)};
+                        screenCloud.push_back(pcl::PointXYZ(point[0], point[1], point[2]));
+                    }
+                }
+
+                // Scanning
+                pcl::PointCloud<pcl::PointXYZ> cloud;
+                vector<double> xx, yy, zz;
+                vector<int> rr, gg, bb;
+                double p_coords[3], x[3], t, rgb[3];
+                int subId;
+                for(int vert = 0 ; vert<scan.res_vert;vert++){
+                    for(int hor = 0 ; hor<scan.res_hor;hor++){
+                        vtkIdType cellId;
+                        double point[3];
+                        point[0] = screenCloud.at(vert*scan.res_hor+hor).x;
+                        point[1] = screenCloud.at(vert*scan.res_hor+hor).y;
+                        point[2] = screenCloud.at(vert*scan.res_hor+hor).z;
+                        if (tree->IntersectWithLine (eye, point, 0, t, x, p_coords, subId, cellId))
+                        {
+                            EstimateColor(data, cellId, x, rgb);
+                            rr.push_back(floor(rgb[0]+0.5));gg.push_back(floor(rgb[1]+0.5));bb.push_back(floor(rgb[2]+0.5));
+                            xx.push_back(x[0]);yy.push_back(x[1]);zz.push_back(x[2]);
+
+                            //vtkMath::Subtract(x,data->GetCenter(),x);
+                            tr->TransformPoint(x,x);
+                            cloud.push_back (PointXYZ(x[0], x[1], x[2]));
+                        }
+                        //else cloud.push_back (PointXYZ(0,0,0));
+                        else{
+                            rr.push_back(0);gg.push_back(0);bb.push_back(0);
+                            xx.push_back(point[0]);yy.push_back(point[1]);zz.push_back(point[2]);
+                            //vtkMath::Subtract(point,data->GetCenter(),point);
+                            tr->TransformPoint(point,point);
+                            cloud.push_back (PointXYZ(point[0],point[1],point[2]));
+                        }
+                    } // Horizontal
+                } // Vertical
+
+                pcl::PCDWriter writer;
+                writer.writeBinaryCompressed(path+"/"+to_string(i)+"_r"+to_string(deg)+"_d"+to_string((int)scan.distance)+".pcd", cloud);
+                ofstream ofs(path+"/"+to_string(i)+"_r"+to_string(deg)+"_d"+to_string((int)scan.distance)+".ply");
+                ofs<<"ply"<<endl;
+                ofs<<"format ascii 1.0"<<endl;
+                ofs<<"comment exported in vitual_scanner"<<endl;
+                ofs<<"element vertex "<<xx.size()<<endl;
+                ofs<<"property float x"<<endl;
+                ofs<<"property float y"<<endl;
+                ofs<<"property float z"<<endl;
+                ofs<<"property uchar red"<<endl;
+                ofs<<"property uchar green"<<endl;
+                ofs<<"property uchar blue"<<endl;
+                ofs<<"end_header"<<endl;
+                for(int idx=0;idx<xx.size();idx++)
+                    ofs<<xx[idx]<<" "<<yy[idx]<<" "<<zz[idx]<<" "<<rr[idx]<<" "<<gg[idx]<<" "<<bb[idx]<<endl;
+                ofs.close();
+            }
         }
     }log.close();
     return 0;
 }
+
 void EstimateColor(vtkPolyData* poly, vtkIdType cellId, double* point, double* rgb){
     double a[3], b[3], c[3];
     poly->GetPoint(poly->GetCell(cellId)->GetPointId(0), a);
